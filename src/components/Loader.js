558e9aff3714fa7e841c83bbcb7a3afb69f94035
Keith Rodriguez <rodriguez.keith@gmail.com>
2021-11-04 13:48:05 -0500

    Styled Loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,19 +3,26 @@ import "./Loader.css";
 const Loader = () => {
   return (
     <div className="loader">
-      <h1>
-        <span className="tilt-left">G</span>
-        <span className="tilt-right">E</span>
-        <span className="tilt-left">T</span>
-      </h1>
-      <h1>
-        <span className="tilt-right">R</span>
-        <span className="tilt-left">E</span>
-        <span className="tilt-right">A</span>
-        <span className="tilt-left">D</span>
-        <span className="tilt-right">Y</span>
-        <span className="tilt-left">!</span>
-      </h1>
+      <div className="message">
+        <h1>
+          <span className="tilt-left">G</span>
+          <span className="tilt-right">E</span>
+          <span className="tilt-left">T</span>
+        </h1>
+        <h1>
+          <span className="tilt-right">R</span>
+          <span className="tilt-left">E</span>
+          <span className="tilt-right">A</span>
+          <span className="tilt-left">D</span>
+          <span className="tilt-right">Y</span>
+          <span className="tilt-left">!</span>
+        </h1>
+      </div>
+
+      <div className="circles">
+        <div className="circle"></div>
+        <div className="circle"></div>
+      </div>
     </div>
   );
 };
